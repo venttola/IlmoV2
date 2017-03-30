66ca06536d05a30532ab8f19708da4653a2337db
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2017-03-30 13:38:08 +0300

    Added the frame for mainpage elements. Also added the mainpage component to application routing

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -3,14 +3,21 @@ import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { AuthGuard } from './authentication/auth-guard.service';
 
 import { LoginPageComponent } from "./login-page/login-page.component";
+import { MainPageComponent } from "./main-page/main-page.component";
 
 const appRoutes: Routes = [
   {
     path: "",
     component: LoginPageComponent
+  },
+  {
+  	path: "main",
+  	component: MainPageComponent,
+  	canActivate: [AuthGuard]
   }
 ];
 
 export const Routing = RouterModule.forRoot(appRoutes);
 
-export const RoutedComponents = [LoginPageComponent];
+export const RoutedComponents = [LoginPageComponent, 
+								 MainPageComponent];
