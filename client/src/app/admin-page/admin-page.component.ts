032682d176bd3977621bbe653f85970406ae8f6d
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2017-04-30 20:48:44 +0300

    Refactored event creator to a separate component. Commit in two parts to restrict git from resolving the refactoring as a rename

--- client/src/app/admin-page/admin-page.component.ts
+++ client/src/app/admin-page/admin-page.component.ts
@@ -1,43 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { Event } from "../event.model";
-import { Platoon } from "../platoon.model";
-import { Organizer } from "../organizer.model";
-import { EventCreatorService } from "./event-creator.service";
+
 @Component({
-	selector: 'app-admin-page',
+	selector: 'admin-page',
 	templateUrl: './admin-page.component.html',
 	styleUrls: ['./admin-page.component.css']
 })
 export class AdminPageComponent implements OnInit {
-	@Input() newEvent: Event;
-	@Input() platoons: Platoon[];
-	@Input() organizer: Organizer;
-	result: any;
-	error: any;
-	constructor(private eventCreatorService: EventCreatorService) { 
-		this.newEvent = new Event;
-		this.platoons = new Array<Platoon>();
-		this.organizer = new Organizer;
+	constructor() { 
 	}
 	ngOnInit() {
 	}
-	createEvent(){
-		this.eventCreatorService.createEvent(this.newEvent, this.platoons, this.organizer).
-		subscribe( function(result){
-			console.log("In AdminPageComponent" + result);
-		}, error => this.error = <any>error);
-	}
-	addInputForPlatoon(){
-		this.platoons.push(new Platoon);
-		console.log(JSON.stringify(this.platoons));
-	}
-	removeInputForPlatoon(){
-		this.platoons.pop();
-		console.log(JSON.stringify(this.platoons));
-	}
-	trackPlatoon(index, item: Platoon){
-		return item.name;
-	}
-
 }
