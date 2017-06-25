f4b789277fef1c1111380d5e9a327bb2b6c19d59
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2017-06-25 22:21:41 +0300

    Switched the migrations to generate sane but ugly reference numbers for groups.

--- migrations/migrations/002-add_referencenumber.js
+++ migrations/migrations/002-add_referencenumber.js
@@ -2,7 +2,7 @@ const FinnishBankUtils = require('finnish-bank-utils')
 
 exports.up = function(next){
   this.addColumn("GroupPayment",{ 
-  	referenceNumber: {type: "text", required: true,  defaultValue: FinnishBankUtils.generateFinnishRefNumber("124")}
+  	referenceNumber: {type: "text", required: true,  defaultValue: FinnishBankUtils.generateFinnishRefNumber()}
   }, next);
 };
 
