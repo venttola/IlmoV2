b695d436e26eecf875b20cc6fb4cbbf9b2811172
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-03-28 01:52:53 +0300

    Added components and services to EventsModule.

--- client/src/app/events/events.module.ts
+++ client/src/app/events/events.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+import { EventListingComponent } from './event-listing/event-listing.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
+import { EventSignupComponent } from './event-signup/event-signup.component';
+import { EventSignupListingComponent } from "./event-signup-listing/event-signup-listing.component"
+import { ProductRowComponent } from './event-signup/product-row/product-row.component';
+import { GroupModalComponent } from './event-details/group-modal/group-modal.component';
+
+import { EventDetailsService } from "./event-details/event-details.service";
+import { EventSignupService } from "./event-signup/event-signup.service";
+
+import { ParticipantGroupService } from "./event-details/participant-group.service";
+import { SignupListingService } from "./event-signup-listing/signup-listing.service";
+
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    EventDetailsComponent,
+    EventListingComponent,
+    EventSignupComponent,
+    EventSignupListingComponent,
+    ProductRowComponent,
+    GroupModalComponent
+  ],
+  providers: [
+    EventListingComponent,
+    EventDetailsService,
+    EventSignupService,
+    ParticipantGroupService,
+    SignupListingService
+  ]
 })
 export class EventsModule { }
