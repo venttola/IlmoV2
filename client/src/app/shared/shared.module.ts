19d4d7d38e5b8b74468de3845771af693940721d
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-03-28 01:46:09 +0300

    Removed moved elements from SharedModule.

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from "@angular/forms";
 
-import { EventService } from './event.service';
-import { EventListingComponent } from './event-listing/event-listing.component';
 
 
 
@@ -12,14 +10,11 @@ import { EventListingComponent } from './event-listing/event-listing.component';
     CommonModule
   ],
   declarations: [
-    EventListingComponent
   ],
   providers: [
-    EventService
+
   ],
   exports: [
-    EventListingComponent,
-    FormsModule
   ]
 })
 export class SharedModule { }
