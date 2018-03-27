304c9451aeaeba076c6b628f04e9831bbc1e1341
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-03-27 21:10:02 +0300

    Implemented SimpleEventListingComponent.

--- client/src/app/admin-dashboard/simple-event-listing/simple-event-listing.component.ts
+++ client/src/app/admin-dashboard/simple-event-listing/simple-event-listing.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { EventService } from "../../shared/event.service";
+import { Event } from "../../shared/event.model";
 @Component({
   selector: 'app-simple-event-listing',
   templateUrl: './simple-event-listing.component.html',
   styleUrls: ['./simple-event-listing.component.css']
 })
 export class SimpleEventListingComponent implements OnInit {
+  events$: Observable<Event[]>;
 
-  constructor() { }
+  private selectedId: number;
+  constructor(
+    private eventService: EventService,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
+    this.events$ = this.route.paramMap
+      .switchMap((params: ParamMap) => {
+        // (+) before `params.get()` turns the string into a number
+        this.selectedId = +params.get('id');
+        return this.eventService.getEventListing();
+      });
   }
 
 }
