178a30c2686c943a856ce8134d8980ffbba5c674
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-03-27 14:13:54 +0300

    Added canActivateChild function to AdminGuard.

--- client/src/app/authentication/admin-guard.service.ts
+++ client/src/app/authentication/admin-guard.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from "@angular/core";
-import { Router, CanActivate } from "@angular/router";
+import { Router, CanActivate, CanActivateChild } from "@angular/router";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AdminGuard implements CanActivate {
+export class AdminGuard implements CanActivate, CanActivateChild {
 
   constructor(private authService: AuthService,
   			  private router: Router) {}
 
   canActivate() {
+    if (!this.authService.isAdmin()) {
+      this.router.navigate(["main"]);
+      return false;
+    }
+    return true;
+  }
+  canActivateChild() {
     if (!this.authService.isAdmin()) {
       this.router.navigate(["main"]);
       return false;
