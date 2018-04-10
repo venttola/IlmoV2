c59e06a77395855dbd69a910fae0167615800adf
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-04-10 23:12:05 +0300

    Added missing module dependencies to GroupModerationModule.

--- client/src/app/group-moderation/group-moderation.module.ts
+++ client/src/app/group-moderation/group-moderation.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
 
-import { GroupModerationRouterModule } from "./group-moderation-router.module"
+import { GroupModerationRouterModule } from "./group-moderation-router.module";
+import { SharedModule } from "../shared/shared.module";
 
 import { GroupListingComponent } from "./group-listing/group-listing.component";
 import { GroupDetailsComponent } from "./group-details/group-details.component";
@@ -13,7 +15,10 @@ import { GroupModerationService } from "./shared/group-moderation.service";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    GroupModerationRouterModule
   ],
   declarations: [
     GroupCheckoutPageComponent,
