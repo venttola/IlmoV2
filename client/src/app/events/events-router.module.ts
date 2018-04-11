9d57e7e728e5a3e8bcbe6b9ff6f62c00a7459fc2
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-04-11 22:30:05 +0300

    Modified EventsModule routing to be in accordance to Angular best practices.

--- client/src/app/events/events-router.module.ts
+++ client/src/app/events/events-router.module.ts
@@ -1,36 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, CanActivate } from '@angular/router';
 
-
+import { EventsComponent } from "./events.component";
 import { EventDetailsComponent } from "./event-details/event-details.component";
 import { EventListingComponent } from "./event-listing/event-listing.component"
-
 import { EventSignupComponent } from "./event-signup/event-signup.component";
 import { EventSignupListingComponent } from "./event-signup-listing/event-signup-listing.component";
 
 import { AuthGuard } from "../authentication/auth-guard.service";
-
+//Fix this
 const adminRoutes: Routes = [
   {
     path: "events",
-    component: EventListingComponent
-  },
-  {
-    path: "events/:id",
-    component: EventDetailsComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: "events/:eventId/group/:groupId",
-    component: EventSignupComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: "signups",
-    component: EventSignupListingComponent,
-    canActivate: [AuthGuard],
-  },
-  
+    component: EventsComponent,
+    children: [
+      {
+        path: "",
+        component: EventListingComponent
+      },
+      {
+        path: ":id",
+        component: EventDetailsComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: ":eventId/group/:groupId",
+        component: EventSignupComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: "signups",
+        component: EventSignupListingComponent,
+        canActivate: [AuthGuard],
+      },
+  ]
+  }
 ];
 
 @NgModule({
--- client/src/app/events/events.module.ts
+++ client/src/app/events/events.module.ts
@@ -6,12 +6,11 @@ import { SharedModule } from "../shared/shared.module";
 
 import { EventsRouterModule } from "./events-router.module";
 
+import { EventsComponent } from "./events.component";
 import { EventListingComponent } from './event-listing/event-listing.component';
 import { EventDetailsComponent } from './event-details/event-details.component';
 import { EventSignupComponent } from './event-signup/event-signup.component';
 import { EventSignupListingComponent } from "./event-signup-listing/event-signup-listing.component"
-//import { ProductRowComponent } from './event-signup/product-row/product-row.component';
-//import { GroupModalComponent } from './event-details/group-modal/group-modal.component';
 
 import { EventDetailsService } from "./event-details/event-details.service";
 import { EventService } from "./shared/event.service";
@@ -29,6 +28,7 @@ import { SignupListingService } from "./event-signup-listing/signup-listing.serv
     EventsRouterModule
   ],
   declarations: [
+    EventsComponent,
     EventDetailsComponent,
     EventListingComponent,
     EventSignupComponent,
