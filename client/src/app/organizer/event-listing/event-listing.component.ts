486f946c3a090cb39fd534a4efc7b4e798a38d4f
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-05-07 22:41:56 +0300

    Added organization's events fetching to EventListingComponent.

--- client/src/app/organizer/event-listing/event-listing.component.ts
+++ client/src/app/organizer/event-listing/event-listing.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
+import { EventListingService } from "./event-listing.service";
+
+import { Event } from "../../events/shared/event.model";
 @Component({
   selector: 'organizer-event-listing',
   templateUrl: './event-listing.component.html',
   styleUrls: ['./event-listing.component.css']
 })
 export class EventListingComponent implements OnInit {
+  events$: Observable<Event[]>;
 
-  constructor() { }
+  private organizationId: number;
+  constructor(
+    private route: ActivatedRoute,
+    private eventListingService: EventListingService
+    ) { }
 
   ngOnInit() {
+    this.events$ = this.route.parent.paramMap
+      .switchMap((params: ParamMap) => {
+        // (+) before `params.get()` turns the string into a number
+        this.organizationId = +params.get('id');
+        return this.eventListingService.getEvents(this.organizationId);
+      });
   }
 
 }
--- client/src/app/organizer/event-listing/event-listing.service.ts
+++ client/src/app/organizer/event-listing/event-listing.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, URLSearchParams } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/catch";
+import "rxjs/add/operator/map";
+import "rxjs/add/observable/forkJoin";
+
+import { Event } from "../../events/shared/event.model";
+import { AuthorizedHttpService } from "../../shared/authorizedhttp.service";
 
 @Injectable()
-export class EventListingService {
-
-  constructor() { }
-
+export class EventListingService extends AuthorizedHttpService {
+  constructor(protected http: Http) {
+    super (http);
+  }
+  getEvents(organizationId: number): Observable<Event[]> {
+    return this.http.get("/api/organizer/" + organizationId + "/events", { headers: this.headers }).map(this.extractData).catch(this.handleError);
+  }
 }
