7e1e38793f39c7623b184b1260cf034343188740
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-05-29 15:42:48 +0300

    Modified Organizationservice.getOrganization to use id as parameter instead of name.

--- src/services/organizationservice.ts
+++ src/services/organizationservice.ts
@@ -5,9 +5,9 @@ module Service {
     export class OrganizationService {
         constructor(private organizationModel: any) { }
 
-        public getOrganization = (name: string) => {
+        public getOrganization = (id: number) => {
             return new Promise((resolve, reject) => {
-                this.organizationModel.one({ name: name }, function (err: Error, organization: any) {
+                this.organizationModel.get(id, function (err: Error, organization: any) {
                     if (err) {
                         let errorMsg = ErrorHandler.getErrorMsg("Organization data", ErrorType.DATABASE_READ);
                         reject(new DatabaseError(500, errorMsg));
