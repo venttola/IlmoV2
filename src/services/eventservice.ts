3f3fc9e61f562d11a5b17d9fa00b0b1239a21896
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-12-09 18:13:49 +0200

    Refactored indentations from 4 t o2 spaces in EventService.

--- src/services/eventservice.ts
+++ src/services/eventservice.ts
@@ -2,71 +2,71 @@
 import { ErrorHandler, ErrorType, APIError, DatabaseError } from "../utils/errorhandler";
 
 module Service {
-    export class EventService {
-        constructor(private eventModel: any) { }
+  export class EventService {
+    constructor(private eventModel: any) { }
 
-        public getEventProducts = (eventId: number) => {
-            return new Promise((resolve, reject) => {
-                this.getEvent(eventId).then((event: any) => {
-                    event.getProducts(function (err: Error, prods: any) {
-                        if (err) {
-                            let errorMsg = ErrorHandler.getErrorMsg("Event product data", ErrorType.DATABASE_READ);
-                            reject(new DatabaseError(500, errorMsg));
-                        } else {
-                            resolve(prods);
-                        }
-                    });
-                });
-            });
-        };
+    public getEventProducts = (eventId: number) => {
+      return new Promise((resolve, reject) => {
+        this.getEvent(eventId).then((event: any) => {
+          event.getProducts(function (err: Error, prods: any) {
+            if (err) {
+              let errorMsg = ErrorHandler.getErrorMsg("Event product data", ErrorType.DATABASE_READ);
+              reject(new DatabaseError(500, errorMsg));
+            } else {
+              resolve(prods);
+            }
+          });
+        });
+      });
+    };
 
-        public isEventRegistrationOpen = (eventId: number) => {
-            return new Promise((resolve, reject) => {
-                this.getEvent(eventId).then((event: any) => {
-                    resolve(event.registerationOpen);
-                });
-            });
-        }
-
-        public getEvent = (eventId: number) => {
-            return new Promise((resolve, reject) => {
-                this.eventModel.one({ id: eventId }, function (err: Error, event: any) {
-                    if (err) {
-                        let errorMsg = ErrorHandler.getErrorMsg("Event data", ErrorType.DATABASE_READ);
-                        reject(new DatabaseError(500, errorMsg));
-                    } else if (!event) {
-                        let errorMsg = ErrorHandler.getErrorMsg("Event", ErrorType.NOT_FOUND);
-                        reject(new DatabaseError(404, errorMsg));
-                    } else {
-                        resolve(event);
-                    }
-                });
-            });
-        }
-        public createEvent = (name: string,
-                              startDate: string,
-                              endDate: string,
-                              description: string,
-                              registerationOpen: boolean) => {
-            return new Promise((resolve, reject) => {
-                this.eventModel.create({
-                name: name,
-                startDate: new Date(startDate),
-                endDate: new Date(endDate),
-                description: description,
-                registerationOpen: registerationOpen
-            }, function (err: Error, event: any) {
-                if (err) {
-                    let errorMsg = ErrorHandler.getErrorMsg("Event data", ErrorType.DATABASE_INSERTION);
-                    return reject(new DatabaseError(500, errorMsg));
-                } else {
-                    return resolve(event);
-                }
-            });
-            });
-        }
-
+    public isEventRegistrationOpen = (eventId: number) => {
+      return new Promise((resolve, reject) => {
+        this.getEvent(eventId).then((event: any) => {
+          resolve(event.registerationOpen);
+        });
+      });
     }
+
+    public getEvent = (eventId: number) => {
+      return new Promise((resolve, reject) => {
+        this.eventModel.one({ id: eventId }, function (err: Error, event: any) {
+          if (err) {
+            let errorMsg = ErrorHandler.getErrorMsg("Event data", ErrorType.DATABASE_READ);
+            reject(new DatabaseError(500, errorMsg));
+          } else if (!event) {
+            let errorMsg = ErrorHandler.getErrorMsg("Event", ErrorType.NOT_FOUND);
+            reject(new DatabaseError(404, errorMsg));
+          } else {
+            resolve(event);
+          }
+        });
+      });
+    }
+    public createEvent = (name: string,
+      startDate: string,
+      endDate: string,
+      description: string,
+      registerationOpen: boolean) => {
+      return new Promise((resolve, reject) => {
+        this.eventModel.create({
+          name: name,
+          startDate: new Date(startDate),
+          endDate: new Date(endDate),
+          description: description,
+          registerationOpen: registerationOpen
+        }, function (err: Error, event: any) {
+          if (err) {
+            let errorMsg = ErrorHandler.getErrorMsg("Event data", ErrorType.DATABASE_INSERTION);
+            return reject(new DatabaseError(500, errorMsg));
+          } else {
+            return resolve(event);
+          }
+        });
+      });
+    }
+
+  }
 }
 
 export = Service;
