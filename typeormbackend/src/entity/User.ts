8da1afeb4cc5c207d65217bcceaeb3ce2114c133
Jukka-Pekka Venttola <jukka-pekka.venttola@gofore.com>
2020-01-01 17:40:16 +0200

    Moved the entities from backend to typeormbackend.

--- typeormbackend/src/entity/User.ts
+++ typeormbackend/src/entity/User.ts
@@ -1,18 +1,47 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable} from "typeorm";
 
+import {ParticipantGroup} from "./ParticipantGroup";
+import {UserPayment} from "./UserPayment";
+import {Organization} from "./Organization";
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    firstName: string;
+  @Column()
+  firstname: string;
 
-    @Column()
-    lastName: string;
+  @Column()
+  lastname: string;
 
-    @Column()
-    age: number;
+  @Column()
+  dob: Date;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  allergies: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  passwordResetToken: string;
+
+  @Column()
+  passwordResetExpires: number;
+
+  @ManyToMany(type => ParticipantGroup, group => group.moderators)
+  @JoinTable()
+  moderatedGroups: ParticipantGroup[];
+
+  @ManyToMany(type => Organization, organization => organization.members)
+  @JoinTable()
+  organizations: Organization[];
 
 }
